1b0c37818513f28caa862d8edd5270031159e8f0
Benny Dreamly <bgaming0277@gmail.com>
2023-12-29 12:15:10 -0700

    ✨moved json folder into resources folder to facilitate easier bundling

--- locations.py
+++ locations.py
@@ -1,25 +1,25 @@
 import json
 
 def load_locations():
-    with open('json/locations.json') as locations_file:
+    with open('res/json/locations.json') as locations_file:
         return json.load(locations_file)
 
 def load_location_names():
-    with open('json/names.json') as names_file:
+    with open('res/json/names.json') as names_file:
         return json.load(names_file)
 
 def load_location_descriptions():
-    with open('json/descriptions.json') as descriptions_file:
+    with open('res/json/descriptions.json') as descriptions_file:
         return json.load(descriptions_file)
 
 def load_commands():
-    with open('json/commands.json') as commands_file:
+    with open('res/json/commands.json') as commands_file:
         return json.load(commands_file)
 
 def load_location_subtitles():
-    with open('json/sub_titles.json') as subtitles_file:
+    with open('res/json/sub_titles.json') as subtitles_file:
         return json.load(subtitles_file)
 
 def load_commands():
-    with open('json/commands.json') as commands_file:
+    with open('res/json/commands.json') as commands_file:
         return json.load(commands_file)
