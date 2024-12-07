6e75d3844f4a1bd4c673038e7df5983af019b31d
Benny Dreamly <bgaming0277@gmail.com>
2024-12-06 17:25:35 -0700

    change save directory

--- adventures-of-dreamland.py
+++ adventures-of-dreamland.py
@@ -96,8 +96,6 @@ def get_save_file_path():
     system = platform.system
     if system == "Windows":
         save_path = Path(os.getenv('LOCALAPPDATA')) / "adventuresofdreamland" / "saves"
-    elif system == "Darwin":
-        save_path = Path.home() / "Library" / "Application Support" / "adventuresofdreamland" / "saves"
     else:
         save_path = Path.home() / ".local" / "share" / "adventuresofdreamland" / "saves"
     save_path.mkdir(parents=True, exist_ok=True)
